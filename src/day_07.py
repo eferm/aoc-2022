776f0f1587b94d84c7b5fe691af327d380623da3
Emanuel Ferm <emanuel@eferm.com>
2022-12-25 13:09:02 +0100

    day 7

---
+++ src/day_07.py
@@ -0,0 +1,53 @@
+import collections
+import pathlib
+import re
+
+from utils import *
+
+
+inp = get_input(2022, 7)
+
+
+def fqfn(d, f):
+    return str(pathlib.PurePath(d, f))
+
+
+def graph(log):
+    children, files = collections.defaultdict(set), dict()
+    parent, pwd = dict(), ""
+
+    for line in log:
+        if match := re.search(r"^\$ cd (.+)$", line):
+            if (new := match.group(1)) != "..":
+                parent[fqfn(pwd, new)] = pwd
+                pwd = fqfn(pwd, new)
+            else:
+                pwd = parent[pwd]
+        elif line == "$ ls":
+            continue
+        else:
+            size, name = line.split(" ")
+            children[pwd].add(fqfn(pwd, name))
+            if size.isnumeric():
+                files[fqfn(pwd, name)] = int(size)
+
+    return children, files
+
+
+def traverse(root):
+    total = 0
+    for c in graph[root]:
+        total += files[c] if c in files else traverse(c)
+    totals[root] = total
+    return total
+
+
+# part 1
+graph, files = graph(inp.split("\n"))
+totals = collections.defaultdict(int)
+traverse("/")
+print(sum(lfilter(lambda size: size < 100_000, totals.values())))
+
+# part 2
+free = 70_000_000 - totals["/"]
+print(min(lfilter(lambda v: (v + free) > 30_000_000, totals.values())))
